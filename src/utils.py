0f35c74d9e2013e5665e9a455930f2dde28dfbd3
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2024-09-26 16:15:04 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,15 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+
+def load_gan_generator(model_path):
+    """Load the pre-trained GAN generator."""
+    return load_model(model_path)
+
+def generate_noise(dim, num_samples):
+    """Generate random noise as input for the GAN."""
+    return np.random.normal(0, 1, (num_samples, dim))
+
+def generate_images(generator, noise):
+    """Generate images using the GAN generator."""
+    return generator.predict(noise)
 
